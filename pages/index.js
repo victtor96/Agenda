7cfd8fc20659e3218f30d16b31c923f566c5b327
victtor96 <56921763+victtor96@users.noreply.github.com>
2021-11-03 20:38:33 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://victornotas.tk/Novembro/semana1"
             className={styles.card}
           >
             <h2>Anotações Gerais</h2>
