66cb19f2f5ebfcf7427cfbda56266f5b079fb388
victtor96 <56921763+victtor96@users.noreply.github.com>
2021-11-03 21:38:53 -0300

    Create p1.js

---
+++ pages/circuitos/p1.js
@@ -0,0 +1,23 @@
+export default function Home() {
+    return (
+      <ul>
+    <li><a href="https://victornotas.tk/">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </li>
+  </ul>
+        
+        
+        
+        
+        
+       
+    )
+  }
+  
